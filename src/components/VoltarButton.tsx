c5bc42ba0068e906433e941bb18ec20dbabf90e3
Max Hayashi Batista <hayashibatista@gmail.com>
2025-09-17 11:22:41 -0300

    feat: estilização do botão voltar

--- src/components/VoltarButton.tsx
+++ src/components/VoltarButton.tsx
@@ -1,20 +1,29 @@
 import { useNavigate } from "react-router-dom";
-
-
+ 
 const VoltarButton = () => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        if (window.history.state && window.history.state.idx > 0) {
-            navigate(-1);
-        } else {
-            navigate("/");
-        }
-        };
-
+  const navigate = useNavigate();
+ 
+  const handleClick = () => {
+    if (window.history.state && window.history.state.idx > 0) {
+      navigate(-1);
+    } else {
+      navigate("/");
+    }
+  };
+ 
   return (
-    <button onClick={handleClick}>Voltar</button>
-  )
-}
-
-export default VoltarButton
+    <div className="flex justify-center py-4">
+      <button
+        onClick={handleClick}
+        className="bg-[#3F58FF] text-white font-semibold px-6 py-3 rounded-md
+                   text-center
+                   transition-all duration-300 ease-in-out
+                   hover:bg-[#0022ff] hover:scale-105"
+      >
+        Voltar
+      </button>
+    </div>
+  );
+};
+ 
+export default VoltarButton;
