678002d817426554deff44fea4439a913560693d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-18 09:05:39 -0300

    feat: componentização dos botões com ButtonWrapper

---
+++ src/components/ButtonWrapper.tsx
@@ -0,0 +1,43 @@
+import type { ReactNode } from "react"
+import { useNavigate } from "react-router-dom"
+ 
+interface WrapperProps {
+  children: ReactNode
+  className?: string
+  to?: string
+  aoClick?: () => void
+}
+ 
+const ButtonWrapper = ({ aoClick, children, className = "", to }: WrapperProps) => {
+  const navigate = useNavigate()
+ 
+  const handleClick = () => {
+    if (to) {
+      navigate(to)
+    }
+    if (aoClick) {
+      aoClick()
+    }
+  }
+ 
+  return (
+    <div className="flex justify-center pb-4">
+      <button
+        onClick={handleClick}
+        className={`
+          bg-gradient-to-r from-[#3F58FF] to-[#0022FF]
+          text-white font-semibold px-6 py-3 rounded-md
+          text-center
+          transition-all duration-200 ease-in-out
+          hover:brightness-125 hover:scale-110
+          shadow-[0_6px_25px_rgba(0,0,0,0.45)]
+          ${className}
+        `}
+      >
+        {children}
+      </button>
+    </div>
+  )
+}
+ 
+export default ButtonWrapper;
--- src/components/VoltarButton.tsx
+++ src/components/VoltarButton.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import ButtonWrapper from "./ButtonWrapper";
+
  
 const VoltarButton = () => {
   const navigate = useNavigate();
@@ -12,21 +14,11 @@ const VoltarButton = () => {
   };
  
   return (
-    <div className="flex justify-center pb-4 ">
-      <button
-        onClick={handleClick}
-        className="
-          bg-gradient-to-r from-[#3F58FF] to-[#0022FF]
-          text-white font-semibold px-6 py-3 rounded-md
-          text-center
-          transition-all duration-200 ease-in-out
-          hover:brightness-120 hover:scale-110
-          shadow-[0_6px_25px_rgba(0,0,0,0.45)]
-        ">
-        Voltar
-      </button>
-    </div>
+    <ButtonWrapper aoClick={handleClick}>
+      Voltar
+    </ButtonWrapper>
   );
 };
  
 export default VoltarButton;
+ 
